ee30c910a10e50d5361e33002a5ba262855d19b6
Gutierrez Gonzalez <d.gutierrez.gonzalez@accenture.com>
2024-03-07 17:50:11 +0100

    En aquest punt ja es pot editar la llibreria i visualitzar els canvis al moment des de un projecte diferent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonComponent } from '../../projects/my-lib/src/lib/button/button.component';
+// import { ButtonComponent, MyLibComponent, TextComponent } from '../../projects/my-lib/src/public-api';
+import { ButtonComponent, MyLibComponent, TextComponent } from '../../dist/my-lib';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent],
+  imports: [RouterOutlet, ButtonComponent, MyLibComponent, TextComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
